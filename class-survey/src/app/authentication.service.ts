d9251a4b3eafe1ab2d563fe2f4330eedf5ad235b
Aurel <aurel@MacBook-Air-di-Aurel.local>
2018-06-21 12:36:19 +0200

    login linked to auth service
    login component linked to authentication service. Small corrections in auth. service.

--- class-survey/src/app/authentication.service.ts
+++ class-survey/src/app/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 //import { LoginComponent } from './login/login.component'
 
@@ -26,12 +27,11 @@ export class AuthenticationService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
 /*
  login(username: string, password: string) {
          return this.http.post<any>('http://localhost:3000/authentication', { username: username, password: password })
--- class-survey/src/app/login/login.component.ts
+++ class-survey/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import {AuthenticationService} from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -11,17 +12,22 @@ export class LoginComponent implements OnInit {
   @Input() private mat: string;
   @Input() private pass: string;
 
+  private esito: any;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
   logIn(): any{
-    return{
+
+    this.authenticationService.login({
       "matriculation": this.mat,
       "password": this.pass
-      };
+    }).subscribe(response => this.esito = response);
+
+    console.log('fatto')
+
     //usa il servizio di login
   }
 
