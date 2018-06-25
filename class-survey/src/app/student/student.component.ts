c2ce3a7964ff36349f557de5b7b33f6a26454ae4
Unknown <m.nesti93@gmail.com>
2018-06-25 16:56:42 +0200

    Student view updated

--- class-survey/src/app/student/student.component.ts
+++ class-survey/src/app/student/student.component.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import {UserDataService} from '../user-data.service';
 
+import {QuesionnaireDataService} from '../quesionnaire-data.service';
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  
-  private user: any;
 
-  constructor(private userDataService: UserDataService) { }
+  private user: any;
+  private questionnaires: any[] = [];
+
+  constructor(private userDataService: UserDataService, private questionnaireDataService: QuesionnaireDataService) { }
 
   ngOnInit() {
     this.getUser()
+    this.getQuestionnaireDetails();
+  }
+
+  getQuestionnaireDetails(){
+    for(let id of this.user.questionnaire){
+      this.questionnaireDataService.retrieveData(id).subscribe((response) => this.checkResponse(response))
+    }
+    console.log(this.questionnaires)
+  }
+
+  checkResponse(response: any) :any{
+    if(!(this.questionnaireDataService.getErrorStatus()===404)){
+        var deadline = response.deadline.split(" - ")
+        delete response.deadline
+        response['date'] = deadline[0]
+        response['time'] = deadline[1]
+        this.questionnaires.push(response);
+    }
+    else{
+      alert('unable to read course details');
+      this.questionnaires.push('404');
+    }
   }
 
   getUser() {
