c38e948ab9bcb497a2827097bef3dfe6f0f6c69c
Unknown <m.nesti93@gmail.com>
2018-06-28 10:32:45 +0200

    saving the inserted answers
    the answer are now stored in a (local) variable

--- class-survey/src/app/compile-questionnaire/compile-questionnaire.component.ts
+++ class-survey/src/app/compile-questionnaire/compile-questionnaire.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import {QuestionnaireDataService} from '../services/questionnaire-data.service';
 import {QuestionDataService} from '../services/question-data.service';
@@ -11,6 +11,10 @@ import {QuestionDataService} from '../services/question-data.service';
 export class CompileQuestionnaireComponent implements OnInit {
   private questionnaire: any;
   private questions: any[] = [];
+
+  @Input() private answers: any[] = [];
+  @Input() private answer: {}; //non utilizzato?
+  @Input() private mul: {} = {};
 
   constructor(private questionnaireDataService: QuestionnaireDataService,
   private questionDataService: QuestionDataService) { }
@@ -39,8 +43,8 @@ export class CompileQuestionnaireComponent implements OnInit {
           delete response.max;*/
           response['range'] = range;
         }
-        console.log('-----')
-        console.log(response)
+        //console.log('-----')
+        //console.log(response)
         this.questions.push(response);
         //console.log(response)
     }
@@ -54,4 +58,39 @@ export class CompileQuestionnaireComponent implements OnInit {
     this.questionnaire = this.questionnaireDataService.getData();
     //this.userDataService.getData().subscribe(user => this.user = user);
   }
+
+  submit(){
+    console.log('submitted!')
+    //console.log(this.answers)
+    var tmp: {} = {}
+    for(let item in this.mul){
+      var id = item.split("-")
+      //tmp[id[1]].push(id[0]);
+      var founded = false
+      for(let k in tmp){
+        if('_'+id[1] == k){
+          founded = true
+        }
+      }
+
+      if(founded == false){
+        tmp['_'+id[1]] = []
+      }
+        tmp['_'+id[1]].push([id[0], this.mul[item]])
+    }
+    for(let item in tmp){
+      this.answers['multiple'+item] = tmp[item];
+    }
+    //console.log(this.mul)
+    //console.log(tmp)
+    console.log(this.answers)
+  }
+/*
+  editCheck(question, option){
+    for(let item in this.tmp){
+      console.log('-----')
+    }
+    console.log(this.tmp)
+    this.answers['multiple_'+question.id] = this.tmp;
+  }*/
 }
