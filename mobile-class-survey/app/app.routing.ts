f5f5786914f016a745ad2798f86973a801615e9f
Aurel <aurel@MacBook-Air-di-Aurel.local>
2018-07-05 17:21:26 +0200

    student component added

--- mobile-class-survey/app/app.routing.ts
+++ mobile-class-survey/app/app.routing.ts
@@ -3,6 +3,7 @@ import { ProfessorComponent } from './professor/professor.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
 import { TemplatesComponent } from './templates/templates.component';
 import { NewQuestionnaireComponent } from './new-questionnaire/new-questionnaire.component';
+import { StudentComponent} from './student/student.component';
 
 export const routes = [
     { path: "", redirectTo: "/login", pathMatch: "full" },
@@ -10,7 +11,8 @@ export const routes = [
     { path: "professor", component: ProfessorComponent },
     { path: "course", component: CourseDetailComponent },
     { path: 'templates', component: TemplatesComponent },
-    { path: "create", component: NewQuestionnaireComponent }
+    { path: "create", component: NewQuestionnaireComponent},
+    { path: "student", component: StudentComponent}
 ];
 
 export const navigatableComponents = [
