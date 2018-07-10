7831cf7f6b3feea33aebabd444502aee65dd5a31
Enrico Civitelli <enrico.civitelli@gmail.com>
2018-07-10 10:56:24 +0200

    Fixed icons bug in mobile-class-survey application
    Also modified package-lock.json in angular-native-seed and mobile-class-survey

--- mobile-class-survey/app/app.module.ts
+++ mobile-class-survey/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
 import { routes, navigatableComponents } from "./app.routing";
 
+import { ServerService } from './services/server.service';
 import { UserDataService } from './services/user-data.service';
 import { CourseDataService } from './services/course-data.service';
 import { AuthenticationService } from './services/authentication.service';
@@ -17,7 +18,6 @@ import { TemplateDataService } from './services/template-data.service';
 import { StatisticsDataService } from './services/statistics-data.service';
 import { QuestionDataService } from './services/question-data.service';
 import { SendAnswerService } from './services/send-answer.service';
-import { ServerService } from './services/server.service';
 
 
 @NgModule({
